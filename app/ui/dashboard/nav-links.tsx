d9115833f857a96db34623e4ef4d353e5eb677d2
KaliMaiCharitableTrust <chandankalimai@gmail.com>
2024-07-02 16:59:35 +0530

    invoices link changed

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -17,7 +17,7 @@ const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
     name: 'Invoices',
-    href: '/dashboard/Invoices',
+    href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
